0a0ecd9067dec989f5b184cdf8a6af5dd53569ef
Vic Patel <vp932712@gmail.com>
2018-01-02 23:51:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |category, options|
+    options.each do |option, pigeons|
+      pigeons.each do |pigeon|
+        if !pigeon_list.include?(pigeon)
+          pigeon_list[pigeon] = {}
+        end
+        if category != :color
+          pigeon_list[pigeon][category] = [option.to_s]
+        elsif pigeon_list[pigeon][category] == nil
+         pigeon_list[pigeon][category] = [option.to_s]
+       else
+          pigeon_list[pigeon][category] << option.to_s
+       end
+      end
+
+end
+  end
+  pigeon_list
 end
